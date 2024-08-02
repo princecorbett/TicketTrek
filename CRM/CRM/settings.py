85fbdfbc197a0555b2a183a2935f93bd07ae0f74
princecorbett <princecorbett3@gmail.com>
2024-08-02 18:42:12 +0530

    dockerfile in making

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,7 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
